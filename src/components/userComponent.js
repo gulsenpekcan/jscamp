f035f4205b4a47379627dafaf5dac1fa30424d27
gulsenpekcan <gulsenpekcan8@gmail.com>
2021-11-09 00:17:46 +0300

    8 Kasım Dersi

---
+++ src/components/userComponent.js
@@ -0,0 +1,22 @@
+import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logging/logger.js"
+import User from "../models/user.js"
+import UserService from "../services/userService.js"
+
+console.log("User component yüklendi.")
+
+let logger1 = new ElasticLogger()
+let userService = new UserService(logger1)
+
+let user1 = new User(1, "Gülşen", "Pekcan", "İstanbul")
+let user2 = new User(2, "Ece", "Cans", "Bursa")
+userService.add(user1)
+userService.add(user2)
+
+console.log(userService.list())
+console.log(userService.getById(2))
+
+//prototyping
+let customer = {id: 1, firstName: "Gülş"}
+customer.lastName = "Pekcan"
+
+console.log(customer.lastName)
