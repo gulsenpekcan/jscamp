1f8a17beec85b29a648e29d7ac956c4952fdb60c
gulsenpekcan <gulsenpekcan8@gmail.com>
2021-11-22 23:39:39 +0300

    22 Kasım Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -12,11 +13,24 @@ let user2 = new User(2, "Ece", "Cans", "Bursa")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 //prototyping
 let customer = {id: 1, firstName: "Gülş"}
 customer.lastName = "Pekcan"
 
 console.log(customer.lastName)
+
+console.log("-------------------------------------------------------------------------------------------------")
+
+userService.load()
+
+let customerToAdd = new Customer(1, "Seço", "Günay", "İstanbul", "dsdasd")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,56 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Gülşen",
+        lastName: "Pekcan",
+        city: "İstanbul",
+        age: 24,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 6,
+        firstName: "Ayco",
+        lastName: "Analist",
+        city: "Fatih",
+        age: 26,
+        creditCardNumber: "123654",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Eto",
+        lastName: "Kıral",
+        city: "İzmir",
+        age: 25,
+        creditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Ecisko",
+        lastName: "Reis",
+        city: "İstanbul",
+        age: 22,
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Cansumi",
+        lastName: "Su",
+        city: "Antalya",
+        age: 18,
+        salary: 10000,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Yzy",
+        lastName: "Çaycı",
+        city: "Rize",
+        age: 26,
+        salary: 10000,
+        type: "employee"
+    }
+]
